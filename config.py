10e1afecc2dbe9a9c7d54580442baef0cb81456a
Трофим <83816117+NolikUss@users.noreply.github.com>
2022-12-14 22:01:52 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 setting = {
     'bot-version': 'beta-test',
-    'token':'MTA0NjA3NzU2MDk4NDk2MTA3NQ.Gx0j2n.PorQeA1xmjvfqbC7SM3tY6VGlyFSB6nHToupKg',
+    'token':'TOP SECRET',
     'botid':'934414589523791893',
     'ownerid': '713437932102615151',
     'testguild': [1046080593173164104],
